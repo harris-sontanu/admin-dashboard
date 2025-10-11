a01129007626d8dafc428370135fc3af15bdf23a
Aldy Nuada <aldynuada132@gmail.com>
2025-10-11 21:20:21 +0800

    fix uplaod image

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -3,9 +3,24 @@
 if (!function_exists('uploadFile')) {
     function uploadFile($file, $folder)
     {
-        $extension  = $file->getClientOriginalExtension();
-        $filename = rand(0, 999) . now()->timestamp . '.' . $extension;
-        $file->move('upload/' . $folder, $filename);
-        return 'upload/' . $folder . '/' . $filename;
+        $path = public_path('upload/' . $folder);
+        if (!file_exists($path)) {
+            mkdir($path, 0755, true);
+        }
+
+        if ($file instanceof \Illuminate\Http\UploadedFile) {
+            $extension = $file->getClientOriginalExtension();
+            $filename = rand(0, 999) . now()->timestamp . '.' . $extension;
+            $file->move($path, $filename);
+            return 'upload/' . $folder . '/' . $filename;
+        }
+
+        if (is_string($file)) {
+            $filename = rand(0, 999) . now()->timestamp . '.webp';
+            file_put_contents($path . '/' . $filename, $file);
+            return 'upload/' . $folder . '/' . $filename;
+        }
+
+        throw new \Exception('Invalid file type for upload');
     }
 }
